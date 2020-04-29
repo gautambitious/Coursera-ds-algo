cbe179f53f2c683878b1d1dfbe13fd22b1ab444b
gautambitious <mvgt.gautam@gmail.com>
2020-04-29 17:40:16 +0530

    Init

---
+++ cmake-build-debug/CMakeFiles/ds_algo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ds_algo.dir/main.cpp.obj"
+  "ds_algo.exe"
+  "ds_algo.exe.manifest"
+  "ds_algo.pdb"
+  "libds_algo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ds_algo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
